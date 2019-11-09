c2671101e8980127e68de9cd830f87b544a0c508
sopehl <semihokanp@gmail.com>
2019-11-09 09:01:44 +0300

    first commit

---
+++ src/main/java/CommandExe.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class CommandExe {
+
+    public static void main(String[] args) throws Exception {
+        String s = "";
+        Process p = Runtime.getRuntime().exec("ls -aF");
+        BufferedReader br = new BufferedReader(
+                new InputStreamReader(p.getInputStream()));
+        while ((s = br.readLine()) != null)
+            System.out.println("line: " + s);
+        p.waitFor();
+        System.out.println ("exit: " + p.exitValue());
+        p.destroy();
+    }
+
+}
