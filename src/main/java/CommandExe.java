1f3835658f270243dcce6ac274cb8ea000962bf6
sopehl <semihokanp@gmail.com>
2019-12-09 23:20:55 +0300

    "[fmerger]"

--- src/main/java/CommandExe.java
+++ src/main/java/CommandExe.java
@@ -2,8 +2,8 @@ public class CommandExe {
 
     public static void main(String[] args) throws Exception {
        ProcessUtils.execute("git status");
-       ProcessUtils.execute("git add .");
-       ProcessUtils.execute("git commit -m fmerger");
+       ProcessUtils.execute("git add --all");
+       ProcessUtils.execute("git commit -m \"[fmerger]\"");
        ProcessUtils.execute("git push https://github.com/sopehl/fmerger-test.git refs/heads/master:refs/heads/master");
        ProcessUtils.execute("git tag");
        ProcessUtils.execute("git push https://github.com/sopehl/fmerger-test.git refs/heads/master:refs/heads/master");
